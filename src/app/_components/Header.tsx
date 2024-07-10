58d26316c5d774f7ad6627d308f93893575a8fdb
Mateus Eduardo <45969051+mateusedua@users.noreply.github.com>
2024-07-10 17:19:30 -0300

    fix: refactor for mobile

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,53 +1,74 @@
 "use client"
 
-import { Avatar, AvatarFallback } from "./ui/avatar"
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
 import { Card } from "./ui/card"
-import { useTheme } from "next-themes"
 import { Button } from "./ui/button"
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger
-} from "./ui/dropdown-menu"
-import { Moon, Sun } from "lucide-react"
+import { Menu } from "lucide-react"
 import Link from "next/link"
+import { Drawer, DrawerContent, DrawerOverlay, DrawerTrigger } from "./ui/drawer"
+import Theme from "./Theme"
+
+const links = [
+    {
+        link: 'Inicio',
+        href: '/'
+    },
+    {
+        link: 'Quem sou',
+        href: '/'
+    },
+    {
+        link: 'Projetos',
+        href: '/'
+    }
+]
 
 const Header = () => {
 
-    const { setTheme } = useTheme()
-
     return (
-        <Card className="p-4 rounded-t-none">
+        <Card className="p-4 rounded-t-none w-full fixed top-0 left-0 z-[999]">
             <div className="flex justify-between items-center">
-                <Link className="flex items-center gap-2" href="/">
+                <div className="lg:hidden md:hidden">
+                    <Drawer>
+                        <DrawerTrigger asChild>
+                            <Button variant="outline" size="icon">
+                                <Menu />
+                            </Button>
+                        </DrawerTrigger>
+                        <DrawerContent>
+                            <div className="mx-auto w-full p-6 flex flex-col">
+                                <div className="flex items-center gap-2">
+                                    <Avatar className="size-16">
+                                        <AvatarImage src="./Logo.jpg" alt="Mateus Eduardo"></AvatarImage>
+                                        <AvatarFallback>MT</AvatarFallback>
+                                    </Avatar>
+                                    <span className="font-bold text-lg">Mateus Eduardo</span>
+                                </div>
+                                <div className="flex flex-col gap-6 text-lg font-bold mt-10">
+                                    {
+                                        links.map((item, index) => (
+                                            <Link href={item.href} key={index} className="text-2xl">{item.link}</Link>
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                        </DrawerContent>
+                    </Drawer>
+                </div>
+                <Link className="flex max-sm:hidden items-center gap-2" href="/">
                     <Avatar className="size-16">
+                        <AvatarImage src="./Logo.jpg" alt="Mateus Eduardo"></AvatarImage>
                         <AvatarFallback>MT</AvatarFallback>
                     </Avatar>
                     <span className="font-bold text-lg">Mateus Eduardo</span>
                 </Link>
                 <div className="flex items-center gap-5">
-                    <ul className="flex gap-5 text-lg font-bold">
+                    <ul className="flex max-sm:hidden gap-5 text-lg font-bold">
                         <li>Inicio</li>
                         <li>Quem sou</li>
+                        <li>Projetos</li>
                     </ul>
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button variant="outline" size="icon">
-                                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                                <span className="sr-only">Toggle theme</span>
-                            </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent align="end">
-                            <DropdownMenuItem onClick={() => setTheme("light")}>
-                                Claro
-                            </DropdownMenuItem>
-                            <DropdownMenuItem onClick={() => setTheme("dark")}>
-                                Escuro
-                            </DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
+                    <Theme />
                 </div>
             </div>
         </Card>
